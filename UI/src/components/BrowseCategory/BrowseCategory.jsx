4da1f19b3a863df38f6f0e0837a0d93f17dd2434
bodhi07 <https://github.com/bodhi07/>
2024-11-10 21:35:52 +0530

    added UI

---
+++ UI/src/components/BrowseCategory/BrowseCategory.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import dresses from '../../assets/img/dresses.jpg'
+
+
+const BrowseCategory = () => {
+  return (
+
+
+    <>
+    
+     <h1 class='text-center text-lg text-blue-400'>Browse By Category</h1>
+
+     <div class="container mx-auto p-20 ">
+  <div class="flex space-x-20">
+    
+    <div class="w-full md:w-1/3 p-9 bg-red-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300  {
+      
+    });">
+      <h2 class="text-xs font-bold text-black-800">Men </h2>
+      <img src="BrowseCategory/assets/img/dresses.jpg" alt="" />
+      
+    </div>
+    
+    
+    <div class="w-full md:w-1/3 p-9 bg-red-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <h2 class="text-xs font-bold text-black-800">Women</h2> 
+    </div>
+    
+    <div class="w-full md:w-1/3 p-9 bg-red-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <h2 class="text-xs font-bold text-black-800">Kids</h2> 
+    </div>
+
+  </div>
+</div>
+
+
+
+
+    
+    </>
+  )
+}
+
+export default BrowseCategory
